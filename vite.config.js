d895704fb3afc30f92239d8b48312134b7c3af1a
doaa <https://github.com/doaa317>
2025-03-17 05:16:26 -0700

    First commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/buttons/", // ✅ تأكد أن اسم المستودع مطابق تمامًا
+  base: "/project/", // ✅ تأكد أن اسم المستودع مطابق تمامًا
   plugins: [react()],
 });
